8bf5fa78eca04d38caf86538637637ab8140aa9c
giuliogallo94 <giuliogallo1994@gmail.com>
2023-11-21 16:42:17 +0100

    add: show list with 10 random mails

--- js/script.js
+++ js/script.js
@@ -2,10 +2,34 @@ const { createApp } = Vue;
 
 const app = createApp({
   data() {
-    return {};
+    return {
+      rndMail: "",
+      mailsArray: [],
+    };
+  },
+  created() {
+    for (i = 0; i < 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((resp) => {
+          console.log(resp.data.response);
+          this.rndMail = resp.data.response;
+          this.mailsArray.push(this.rndMail);
+          console.log(this.mailsArray);
+        });
+    }
   },
 
-  methods: {},
+  methods: {
+    // generateMail: function () {
+    //   axios
+    //     .get("https://flynn.boolean.careers/exercises/api/random/mail")
+    //     .then((resp) => {
+    //       console.log(resp.data.response);
+    //       this.rndMail = resp.data.response;
+    //     });
+    // },
+  },
 });
 
 app.mount("#app");
