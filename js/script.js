3cb7270a2dfd19cfed938ec4ee7036a655ce84a0
giuliogallo94 <giuliogallo1994@gmail.com>
2023-11-22 10:22:16 +0100

    add: button to generate mails, and v-if "loading"

--- js/script.js
+++ js/script.js
@@ -4,19 +4,35 @@ const app = createApp({
   data() {
     return {
       mailsArray: [],
+      isLoading: false,
     };
   },
   created() {
-    for (i = 0; i < 10; i++) {
-      axios
-        .get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then((resp) => {
-          this.mailsArray.push(resp.data.response);
-        });
-    }
+    // for (i = 0; i < 10; i++) {
+    //   axios
+    //     .get("https://flynn.boolean.careers/exercises/api/random/mail")
+    //     .then((resp) => {
+    //       this.mailsArray.push(resp.data.response);
+    //     });
   },
 
-  methods: {},
+  methods: {
+    generateEmails: function () {
+      this.mailsArray = [];
+      this.isLoading = true;
+      for (i = 0; i < 10; i++) {
+        axios
+          .get("https://flynn.boolean.careers/exercises/api/random/mail")
+          .then((resp) => {
+            console.log(resp.data.response);
+            this.mailsArray.push(resp.data.response);
+            if (this.mailsArray.length === 10) {
+              this.isLoading = false;
+            }
+          });
+      }
+    },
+  },
 });
 
 app.mount("#app");
